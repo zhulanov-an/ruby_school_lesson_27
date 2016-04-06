5aebfde5bdf60a062128fa5fb3e6c12bce63db8d
Zhulanov Anton <zhulanov-an@dartit.ru>
2016-04-06 16:02:06 +0500

    add handler to new post

--- app.rb
+++ app.rb
@@ -13,3 +13,9 @@ end
 get '/new_post' do
   erb :new_post
 end
+
+post '/new' do
+  @post_name = params[:post_name]
+  @post_text = params[:post_text]
+  erb "post #{@post_name} post_text #{@post_text}"
+end
